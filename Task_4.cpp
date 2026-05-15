96992af3fec0218d08b58e0ab8f7a83b99b32f9d
Mansi Parekh <mansiparekh1303@gmail.com>
2026-05-15 19:41:29 +0530

    Create Task_4.cpp
    This is one part of mini project,will post rest of two soon.

---
+++ Task_4.cpp
@@ -0,0 +1,91 @@
+#include<iostream>
+using namespace std;
+
+class UnitConverter
+{
+    int choice;
+
+public:
+
+    void MenuBasedInput()
+    {
+        cout << "===== UNIT CONVERTER =====" << endl;
+        cout << "1. Kilometer To Meter" << endl;
+        cout << "2. Meter To Kilometer" << endl;
+        cout << "3. Centimetre To Meter" << endl;
+
+        cout << "Enter Your Choice : ";
+        cin >> choice;
+
+        switch(choice)
+        {
+            case 1:
+                KilometreToMeter();
+                break;
+
+            case 2:
+                MeterToKilometre();
+                break;
+
+            case 3:
+                CentimetreToMeter();
+                break;
+
+            default:
+                cout << "Invalid Choice";
+        }
+    }
+
+    void KilometreToMeter()
+    {
+        cout << "\nConverting Kilometer To Meter" << endl;
+
+        int value, meter;
+
+        cout << "Enter Value To Convert : ";
+        cin >> value;
+
+        meter = value * 1000;
+
+        cout << value << " km = " << meter << " meter";
+    }
+
+    void MeterToKilometre()
+    {
+        cout << "\nConverting Meter To Kilometer" << endl;
+
+        int value;
+        float kilometer;
+
+        cout << "Enter Value To Convert : ";
+        cin >> value;
+
+        kilometer = value / 1000.0;
+
+        cout << value << " meter = " << kilometer << " km";
+    }
+
+    void CentimetreToMeter()
+    {
+        cout << "\nConverting Centimetre To Meter" << endl;
+
+        int value;
+        float meter;
+
+        cout << "Enter Value To Convert : ";
+        cin >> value;
+
+        meter = value / 100.0;
+
+        cout << value << " cm = " << meter << " meter";
+    }
+};
+
+int main()
+{
+    UnitConverter obj;
+
+    obj.MenuBasedInput();
+
+    return 0;
+}
